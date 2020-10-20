b9a22a464e24ed06006a188c4bc6a4fced8fe0bd
jeanTrindade <jeanctrindade@outlook.com>
2020-10-19 22:32:16 -0300

    primeiro commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+function calcular(){
+    let lata = document.getElementById("lata");
+    let vidro = document.getElementById("vidro");
+    let papel = document.getElementById("papel");
+    let garrafa = document.getElementById("garrafa");
+    let sv = Number(document.getElementById("Sv"));
+    let saida = document.getElementById("saida");
+    let mes = document.getElementById("mes");
+    let ano = document.getElementById("ano");
+
+    
+    saida.innerHTML = sv;
+
+}
